6c2779273e60179e0177f6da86376a3dcd69d671
Malik Ashebani <malikashebani@gmail.com>
2019-11-18 08:43:19 +0200

    Revert "Classified the InstructionSet"
    This reverts commit 183fdbe9c86240604800f31d14b90f38c85a1dce.

--- SmokeStackVM/VMProcessor.fs
+++ SmokeStackVM/VMProcessor.fs
@@ -6,58 +6,31 @@ module VMProcessor =
         | RegisterOp
         | NoneOp
 
-    type InstructionClass =
-        | StackOp
-        | ArithmeticOp
-        | BinaryOp
-        | ComparsionOp
-        | BranchOp
-        | DataMovementOp
-        | UnknownOp
-
     type InstructionSpec = {
         Opcode: byte
         Mnemonic: string
         OperandType: OperandType
-        InstructionClass: InstructionClass
     }
 
 open VMProcessor
 module VMProcessorSpec =
-    type Register =
+    type Registers =
         | PC = 0
         | SP = 1
         | R1 = 2
         | R2 = 3
 
-    let PushInstruction : InstructionSpec = { Opcode = 0uy; Mnemonic = "push"; OperandType = ImmediateOp; InstructionClass = StackOp; }
-    let PopInstruction : InstructionSpec = { Opcode = 1uy; Mnemonic = "pop"; OperandType = NoneOp; InstructionClass = StackOp }
-    let AddInstruction : InstructionSpec = { Opcode = 2uy; Mnemonic = "add"; OperandType = NoneOp; InstructionClass = ArithmeticOp; }
-    let SubInstruction : InstructionSpec = { Opcode = 3uy; Mnemonic = "sub"; OperandType = NoneOp; InstructionClass = ArithmeticOp; }
-    let RorInstruction : InstructionSpec = { Opcode = 4uy; Mnemonic = "ror"; OperandType = NoneOp; InstructionClass = BinaryOp; }
-    let RolInstruction : InstructionSpec = { Opcode = 5uy; Mnemonic = "rol"; OperandType = NoneOp; InstructionClass = BinaryOp; }
-    let XorInstruction : InstructionSpec = { Opcode = 6uy; Mnemonic = "xor"; OperandType = NoneOp; InstructionClass = BinaryOp; }
-    let NotInstruction : InstructionSpec = { Opcode = 7uy; Mnemonic = "not"; OperandType = NoneOp; InstructionClass = BinaryOp; }
-    let EqInstruction : InstructionSpec = { Opcode = 8uy; Mnemonic = "eq"; OperandType = NoneOp; InstructionClass = ComparsionOp; }
-    let IfInstruction : InstructionSpec = { Opcode = 9uy; Mnemonic = "if"; OperandType = NoneOp; InstructionClass = ComparsionOp; }
-    let BrInstruction : InstructionSpec = { Opcode = 10uy; Mnemonic = "br"; OperandType = NoneOp; InstructionClass = BranchOp; }
-    let StoreInstruction : InstructionSpec = { Opcode = 11uy; Mnemonic = "store"; OperandType = RegisterOp; InstructionClass = DataMovementOp; }
-    let LoadInstruction : InstructionSpec = { Opcode = 12uy; Mnemonic = "load"; OperandType = RegisterOp; InstructionClass = DataMovementOp; }
-    let NopInstruction : InstructionSpec = { Opcode = 13uy; Mnemonic = "nop"; OperandType = NoneOp; InstructionClass = UnknownOp; }
-
-    let VMInstructionSet : InstructionSpec [] = [|
-        PushInstruction
-        PopInstruction
-        AddInstruction
-        SubInstruction
-        RorInstruction
-        RolInstruction
-        XorInstruction
-        NotInstruction
-        EqInstruction
-        IfInstruction
-        BrInstruction
-        StoreInstruction
-        LoadInstruction
-        NopInstruction
-    |]
+    let PushInstruction : InstructionSpec = { Opcode = 0uy; Mnemonic = "push"; OperandType = ImmediateOp; }
+    let PopInstruction : InstructionSpec = { Opcode = 1uy; Mnemonic = "pop"; OperandType = NoneOp; }
+    let AddInstruction : InstructionSpec = { Opcode = 2uy; Mnemonic = "add"; OperandType = NoneOp; }
+    let SubInstruction : InstructionSpec = { Opcode = 3uy; Mnemonic = "sub"; OperandType = NoneOp; }
+    let RorInstruction : InstructionSpec = { Opcode = 4uy; Mnemonic = "ror"; OperandType = NoneOp; }
+    let RolInstruction : InstructionSpec = { Opcode = 5uy; Mnemonic = "rol"; OperandType = NoneOp; }
+    let XorInstruction : InstructionSpec = { Opcode = 6uy; Mnemonic = "xor"; OperandType = NoneOp; }
+    let NotInstruction : InstructionSpec = { Opcode = 7uy; Mnemonic = "not"; OperandType = NoneOp; }
+    let EqInstruction : InstructionSpec = { Opcode = 8uy; Mnemonic = "eq"; OperandType = NoneOp; }
+    let IfInstruction : InstructionSpec = { Opcode = 9uy; Mnemonic = "if"; OperandType = NoneOp; }
+    let BrInstruction : InstructionSpec = { Opcode = 10uy; Mnemonic = "br"; OperandType = NoneOp; }
+    let StoreInstruction : InstructionSpec = { Opcode = 11uy; Mnemonic = "store"; OperandType = RegisterOp; }
+    let LoadInstruction : InstructionSpec = { Opcode = 12uy; Mnemonic = "load"; OperandType = RegisterOp; }
+    let NopInstruction : InstructionSpec = { Opcode = 13uy; Mnemonic = "nop"; OperandType = NoneOp; }
