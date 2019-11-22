859df53864c7103abb310c3532aaaffbbba2e583
Malik Ashebani <malikashebani@gmail.com>
2019-11-22 08:45:27 +0200

    Created a test for 'VMDisassembler.DisassembleLinearly' function

--- SmokeStackVM.Tests/VMDisassemblerTestSuite.fs
+++ SmokeStackVM.Tests/VMDisassemblerTestSuite.fs
@@ -1,6 +1,10 @@
 ﻿module VMDisassemblerTestSuite
     open Testing
+    open System
 
+    open SmokeStackVM.VMProcessorSpec
+    open SmokeStackVM.VMDisassembler
+    [<Flags>]
     type VMDisassemblerTestCaseCategory =
         | CorrectlyEncodedInstruction
         | IncorrectlyEncodedInstruction
@@ -8,22 +12,32 @@
         | IncorrectlyDisassembledInstruction
 
     let DisassembleFunctionTestCases : TestCase<byte[], VMDisassemblerTestCaseCategory> [] = [|
-        { Caption = "PushInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x00uy; 0x01uy |] }
-        { Caption = "PushInstruction"; Category = IncorrectlyEncodedInstruction; Data = [| 0x00uy; |] }
-        { Caption = "PopInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x01uy; 0x01uy |] }
-        { Caption = "PopInstruction"; Category = IncorrectlyEncodedInstruction; Data = [| 0x01uy; |] }
-        { Caption = "AddInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x02uy |] }
-        { Caption = "SubInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x03uy |] }
-        { Caption = "RorInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x04uy |] }
-        { Caption = "RolInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x05uy |] }
-        { Caption = "XorInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x06uy |] }
-        { Caption = "NotInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x07uy |] }
-        { Caption = "EqInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x08uy |] }
-        { Caption = "IfInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x09uy |] }
-        { Caption = "BrInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x0Auy |] }
-        { Caption = "StoreInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x0Buy; 0x01uy |] }
-        { Caption = "StoreInstruction"; Category = IncorrectlyEncodedInstruction; Data = [| 0x0Buy; |] }
-        { Caption = "LoadInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x0Cuy; 0x01uy |] }
-        { Caption = "LoadInstruction"; Category = IncorrectlyEncodedInstruction; Data = [| 0x0Cuy; |] }
-        { Caption = "NopInstruction"; Category = CorrectlyEncodedInstruction; Data = [| 0x0Duy |] }
+        { Caption = "PushEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x00uy; 0x01uy |] }
+        { Caption = "PushEncoded"; Category = IncorrectlyEncodedInstruction; Data = [| 0x00uy; |] }
+        { Caption = "PopEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x01uy; 0x01uy |] }
+        { Caption = "PopEncoded"; Category = IncorrectlyEncodedInstruction; Data = [| 0x01uy; |] }
+        { Caption = "AddEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x02uy |] }
+        { Caption = "SubEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x03uy |] }
+        { Caption = "RorEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x04uy |] }
+        { Caption = "RolEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x05uy |] }
+        { Caption = "XorEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x06uy |] }
+        { Caption = "NotEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x07uy |] }
+        { Caption = "EqEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x08uy |] }
+        { Caption = "IfEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x09uy |] }
+        { Caption = "BrEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x0Auy |] }
+        { Caption = "StoreEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x0Buy; 0x01uy |] }
+        { Caption = "StoreEncoded"; Category = IncorrectlyEncodedInstruction; Data = [| 0x0Buy; |] }
+        { Caption = "LoadEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x0Cuy; 0x01uy |] }
+        { Caption = "LoadEncoded"; Category = IncorrectlyEncodedInstruction; Data = [| 0x0Cuy; |] }
+        { Caption = "NopEncoded"; Category = CorrectlyEncodedInstruction; Data = [| 0x0Duy |] }
+    |]
+
+    let DisassembleLinearlyFunctionTestCase : TestCase<DisassembledInstruction, VMDisassemblerTestCaseCategory> [] = [|
+        { Caption = "PushDisassmebled"; Category = CorrectlyDisassembledInstruction; Data = { Instruction = PushInstruction; Operand = "1"; Address = 0u; Blob = [| 0x00uy; 0x01uy |] } }
+        { Caption = "AddDisassmebled"; Category = CorrectlyDisassembledInstruction; Data = { Instruction = AddInstruction; Operand = ""; Address = 2u; Blob = [| 0x02uy |] } }
+        { Caption = "RorDisassmebled"; Category = CorrectlyDisassembledInstruction; Data = { Instruction = RorInstruction; Operand = ""; Address = 3u; Blob = [| 0x04uy |] } }
+        { Caption = "EqDisassmebled"; Category = CorrectlyDisassembledInstruction; Data = { Instruction = EqInstruction; Operand = ""; Address = 4u; Blob = [| 0x08uy |] } }
+        { Caption = "BrDisassmebled"; Category = CorrectlyDisassembledInstruction; Data = { Instruction = BrInstruction; Operand = ""; Address = 5u; Blob = [| 0x0Auy |] } }
+        { Caption = "StoreDisassmebled"; Category = CorrectlyDisassembledInstruction; Data = { Instruction = StoreInstruction; Operand = "r2"; Address = 6u; Blob = [| 0x0Buy; 0x01uy |] } }
+        { Caption = "NopDisassmebled"; Category = CorrectlyDisassembledInstruction; Data = { Instruction = NopInstruction; Operand = ""; Address = 8u; Blob = [| 0x0Duy |] } }
     |]
--- SmokeStackVM.Tests/VMDisassemblerTests.fs
+++ SmokeStackVM.Tests/VMDisassemblerTests.fs
@@ -10,8 +10,8 @@
 
     [<Fact>]
     let ``Push Instruction is disassembled correctly`` () =
-        let CorrectPushTestCase = (DisassembleFunctionTestCases
-                        |> Array.filter (fun testCase -> testCase.Caption = "PushInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectPushTestCase = Array.head (DisassembleFunctionTestCases
+                        |> Array.filter (fun testCase -> testCase.Caption = "PushEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledPush = Disassemble (CorrectPushTestCase.Data, 0x00u)
         Assert.True(DisassembledPush.Instruction.Opcode = PushInstruction.Opcode
         && DisassembledPush.Operand = CorrectPushTestCase.Data.[1].ToString()
@@ -19,8 +19,8 @@
 
     [<Fact>]
     let ``Pop Instruction is disassembled correctly`` () =
-        let CorrectPopTestCase = (DisassembleFunctionTestCases
-                    |> Array.filter (fun testCase -> testCase.Caption = "PopInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectPopTestCase = Array.head (DisassembleFunctionTestCases
+                    |> Array.filter (fun testCase -> testCase.Caption = "PopEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledPop = Disassemble (CorrectPopTestCase.Data, 0x00u)
         Assert.True(DisassembledPop.Instruction.Opcode = PopInstruction.Opcode
         && DisassembledPop.Operand = CorrectPopTestCase.Data.[1].ToString()
@@ -28,71 +28,71 @@
     
     [<Fact>]
     let ``Add Instruction is disassembled correctly`` () =
-        let CorrectAddTestCase = (DisassembleFunctionTestCases
-                    |> Array.filter (fun testCase -> testCase.Caption = "AddInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectAddTestCase = Array.head (DisassembleFunctionTestCases
+                    |> Array.filter (fun testCase -> testCase.Caption = "AddEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledAdd = Disassemble (CorrectAddTestCase.Data, 0x00u)
         Assert.True(DisassembledAdd.Instruction.Opcode = AddInstruction.Opcode)
 
     [<Fact>]
     let ``Sub Instruction is disassembled correctly`` () =
-        let CorrectSubTestCase = (DisassembleFunctionTestCases
-                    |> Array.filter (fun testCase -> testCase.Caption = "SubInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectSubTestCase = Array.head (DisassembleFunctionTestCases
+                    |> Array.filter (fun testCase -> testCase.Caption = "SubEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledSub = Disassemble (CorrectSubTestCase.Data, 0x00u)
         Assert.True(DisassembledSub.Instruction.Opcode = SubInstruction.Opcode)
 
     [<Fact>]
     let ``Ror Instruction is disassembled correctly`` () =
-        let CorrectRorTestCase = (DisassembleFunctionTestCases
-                    |> Array.filter (fun testCase -> testCase.Caption = "RorInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectRorTestCase = Array.head (DisassembleFunctionTestCases
+                    |> Array.filter (fun testCase -> testCase.Caption = "RorEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledRor = Disassemble (CorrectRorTestCase.Data, 0x00u)
         Assert.True(DisassembledRor.Instruction.Opcode = RorInstruction.Opcode)
 
     [<Fact>]
     let ``Rol Instruction is disassembled correctly`` () =
-        let CorrectRolTestCase = (DisassembleFunctionTestCases
-                    |> Array.filter (fun testCase -> testCase.Caption = "RolInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectRolTestCase = Array.head (DisassembleFunctionTestCases
+                    |> Array.filter (fun testCase -> testCase.Caption = "RolEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledRol = Disassemble (CorrectRolTestCase.Data, 0x00u)
         Assert.True(DisassembledRol.Instruction.Opcode = RolInstruction.Opcode)
 
     [<Fact>]
     let ``Xor Instruction is disassembled correctly`` () =
-        let CorrectXorTestCase = (DisassembleFunctionTestCases
-                    |> Array.filter (fun testCase -> testCase.Caption = "XorInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectXorTestCase = Array.head (DisassembleFunctionTestCases
+                    |> Array.filter (fun testCase -> testCase.Caption = "XorEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledXor = Disassemble (CorrectXorTestCase.Data, 0x00u)
         Assert.True(DisassembledXor.Instruction.Opcode = XorInstruction.Opcode)
 
     [<Fact>]
     let ``Not Instruction is disassembled correctly`` () =
-        let CorrectNotTestCase = (DisassembleFunctionTestCases
-                    |> Array.filter (fun testCase -> testCase.Caption = "NotInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectNotTestCase = Array.head (DisassembleFunctionTestCases
+                    |> Array.filter (fun testCase -> testCase.Caption = "NotEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledNot = Disassemble (CorrectNotTestCase.Data, 0x00u)
         Assert.True(DisassembledNot.Instruction.Opcode = NotInstruction.Opcode)
 
     [<Fact>]
     let ``Eq Instruction is disassembled correctly`` () =
-        let CorrectEqTestCase = (DisassembleFunctionTestCases
-                |> Array.filter (fun testCase -> testCase.Caption = "EqInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectEqTestCase = Array.head (DisassembleFunctionTestCases
+                    |> Array.filter (fun testCase -> testCase.Caption = "EqEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledEq = Disassemble (CorrectEqTestCase.Data, 0x00u)
         Assert.True(DisassembledEq.Instruction.Opcode = EqInstruction.Opcode)
 
     [<Fact>]
     let ``If Instruction is disassembled correctly`` () =
-        let CorrectIfTestCase = (DisassembleFunctionTestCases
-                |> Array.filter (fun testCase -> testCase.Caption = "IfInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectIfTestCase = Array.head (DisassembleFunctionTestCases
+                    |> Array.filter (fun testCase -> testCase.Caption = "IfEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledIf = Disassemble (CorrectIfTestCase.Data, 0x00u)
         Assert.True(DisassembledIf.Instruction.Opcode = IfInstruction.Opcode)
 
     [<Fact>]
     let ``Br Instruction is disassembled correctly`` () =
-        let CorrectBrTestCase = (DisassembleFunctionTestCases
-                |> Array.filter (fun testCase -> testCase.Caption = "BrInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectBrTestCase = Array.head (DisassembleFunctionTestCases
+                    |> Array.filter (fun testCase -> testCase.Caption = "BrEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledBr = Disassemble (CorrectBrTestCase.Data, 0x00u)
         Assert.True(DisassembledBr.Instruction.Opcode = BrInstruction.Opcode)
 
     [<Fact>]
     let ``Store Instruction is disassembled correctly`` () =
-        let CorrectStoreTestCase = (DisassembleFunctionTestCases
-                    |> Array.filter (fun testCase -> testCase.Caption = "StoreInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectStoreTestCase = Array.head (DisassembleFunctionTestCases
+                    |> Array.filter (fun testCase -> testCase.Caption = "StoreEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledStore = Disassemble (CorrectStoreTestCase.Data, 0x00u)
         Assert.True(DisassembledStore.Instruction.Opcode = StoreInstruction.Opcode
         && DisassembledStore.Operand = enum<Register>(int32 CorrectStoreTestCase.Data.[1]).ToString()
@@ -100,8 +100,8 @@
 
     [<Fact>]
     let ``Load Instruction is disassembled correctly`` () =
-        let CorrectLoadTestCase = (DisassembleFunctionTestCases
-                    |> Array.filter (fun testCase -> testCase.Caption = "LoadInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectLoadTestCase = Array.head (DisassembleFunctionTestCases
+                    |> Array.filter (fun testCase -> testCase.Caption = "LoadEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledLoad = Disassemble (CorrectLoadTestCase.Data, 0x00u)
         Assert.True(DisassembledLoad.Instruction.Opcode = LoadInstruction.Opcode
         && DisassembledLoad.Operand = enum<Register>(int32 CorrectLoadTestCase.Data.[1]).ToString()
@@ -109,7 +109,15 @@
 
     [<Fact>]
     let ``Nop Instruction is disassembled correctly`` () =
-        let CorrectNopTestCase = (DisassembleFunctionTestCases
-                    |> Array.filter (fun testCase -> testCase.Caption = "NopInstruction" && testCase.Category = CorrectlyEncodedInstruction)).[0]
+        let CorrectNopTestCase = Array.head (DisassembleFunctionTestCases
+                    |> Array.filter (fun testCase -> testCase.Caption = "NopEncoded" && testCase.Category = CorrectlyEncodedInstruction))
         let DisassembledNop = Disassemble (CorrectNopTestCase.Data, 0x00u)
         Assert.True(DisassembledNop.Instruction.Opcode = NopInstruction.Opcode)
+
+    [<Fact>]
+    let ``Linear sweep disassembly algorithm is implemented correctly`` () =
+        let TestCaseData = Array.concat (Seq.ofArray (DisassembleLinearlyFunctionTestCase |> Array.choose (fun testCase -> Some (testCase.Data.Blob))))
+        let TestCaseDisassembly = List.ofArray (DisassembleLinearlyFunctionTestCase |> Array.choose (fun testCase -> Some (testCase.Data)))
+        let ImplementationResult = DisassembleLinearly (TestCaseData)
+        let DisassemblyOutputsAreIdentical = List.forall2 (fun disasm1 disasm2 -> disasm1 = disasm2) TestCaseDisassembly ImplementationResult
+        Assert.True(DisassemblyOutputsAreIdentical)
