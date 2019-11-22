ea4832bed6865bc3cbfeda52d350e44146eed9df
Malik Ashebani <malikashebani@gmail.com>
2019-11-22 18:43:25 +0200

    Added length checks and error handling to 'Disassemble' and 'DisassembleLinearly' functions

--- SmokeStackVM.Tests/VMDisassemblerTests.fs
+++ SmokeStackVM.Tests/VMDisassemblerTests.fs
@@ -12,112 +12,157 @@
     let ``Push Instruction is disassembled correctly`` () =
         let CorrectPushTestCase = Array.head (DisassembleFunctionTestCases
                         |> Array.filter (fun testCase -> testCase.Caption = "PushEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledPush = Disassemble (CorrectPushTestCase.Data, 0x00u)
-        Assert.True(DisassembledPush.Instruction.Opcode = PushInstruction.Opcode
-        && DisassembledPush.Operand = CorrectPushTestCase.Data.[1].ToString()
-        )
+        let DisasmOutput = Disassemble (CorrectPushTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledPush ->
+                Assert.True(disassembledPush.Instruction.Opcode = PushInstruction.Opcode
+                && disassembledPush.Operand = CorrectPushTestCase.Data.[1].ToString()
+                )
+            | Error errorMessage -> Assert.True(false)
 
     [<Fact>]
     let ``Pop Instruction is disassembled correctly`` () =
         let CorrectPopTestCase = Array.head (DisassembleFunctionTestCases
                     |> Array.filter (fun testCase -> testCase.Caption = "PopEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledPop = Disassemble (CorrectPopTestCase.Data, 0x00u)
-        Assert.True(DisassembledPop.Instruction.Opcode = PopInstruction.Opcode
-        && DisassembledPop.Operand = CorrectPopTestCase.Data.[1].ToString()
-        )
+        let DisasmOutput = Disassemble (CorrectPopTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledPop ->
+                Assert.True(disassembledPop.Instruction.Opcode = PopInstruction.Opcode
+                && disassembledPop.Operand = CorrectPopTestCase.Data.[1].ToString()
+                )
+            | Error errorMessage -> Assert.True(false)
     
     [<Fact>]
     let ``Add Instruction is disassembled correctly`` () =
         let CorrectAddTestCase = Array.head (DisassembleFunctionTestCases
                     |> Array.filter (fun testCase -> testCase.Caption = "AddEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledAdd = Disassemble (CorrectAddTestCase.Data, 0x00u)
-        Assert.True(DisassembledAdd.Instruction.Opcode = AddInstruction.Opcode)
+        let DisasmOutput = Disassemble (CorrectAddTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledAdd ->
+                Assert.True(disassembledAdd.Instruction.Opcode = AddInstruction.Opcode)
+            | Error errorMessage -> Assert.True(false)
 
     [<Fact>]
     let ``Sub Instruction is disassembled correctly`` () =
         let CorrectSubTestCase = Array.head (DisassembleFunctionTestCases
                     |> Array.filter (fun testCase -> testCase.Caption = "SubEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledSub = Disassemble (CorrectSubTestCase.Data, 0x00u)
-        Assert.True(DisassembledSub.Instruction.Opcode = SubInstruction.Opcode)
+        let DisasmOutput = Disassemble (CorrectSubTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledSub ->
+                Assert.True(disassembledSub.Instruction.Opcode = SubInstruction.Opcode)
+            | Error errorMessage -> Assert.True(false)
 
     [<Fact>]
     let ``Ror Instruction is disassembled correctly`` () =
         let CorrectRorTestCase = Array.head (DisassembleFunctionTestCases
                     |> Array.filter (fun testCase -> testCase.Caption = "RorEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledRor = Disassemble (CorrectRorTestCase.Data, 0x00u)
-        Assert.True(DisassembledRor.Instruction.Opcode = RorInstruction.Opcode)
+        let DisasmOutput = Disassemble (CorrectRorTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledRor ->
+                Assert.True(disassembledRor.Instruction.Opcode = RorInstruction.Opcode)
+            | Error errorMessage -> Assert.True(false)
 
     [<Fact>]
     let ``Rol Instruction is disassembled correctly`` () =
         let CorrectRolTestCase = Array.head (DisassembleFunctionTestCases
                     |> Array.filter (fun testCase -> testCase.Caption = "RolEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledRol = Disassemble (CorrectRolTestCase.Data, 0x00u)
-        Assert.True(DisassembledRol.Instruction.Opcode = RolInstruction.Opcode)
+        let DisasmOutput = Disassemble (CorrectRolTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledRol ->
+                Assert.True(disassembledRol.Instruction.Opcode = RolInstruction.Opcode)
+            | Error errorMessage -> Assert.True(false)
 
     [<Fact>]
     let ``Xor Instruction is disassembled correctly`` () =
         let CorrectXorTestCase = Array.head (DisassembleFunctionTestCases
                     |> Array.filter (fun testCase -> testCase.Caption = "XorEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledXor = Disassemble (CorrectXorTestCase.Data, 0x00u)
-        Assert.True(DisassembledXor.Instruction.Opcode = XorInstruction.Opcode)
+        let DisasmOutput = Disassemble (CorrectXorTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledXor ->
+                Assert.True(disassembledXor.Instruction.Opcode = XorInstruction.Opcode)
+            | Error errorMessage -> Assert.True(false)
 
     [<Fact>]
     let ``Not Instruction is disassembled correctly`` () =
         let CorrectNotTestCase = Array.head (DisassembleFunctionTestCases
                     |> Array.filter (fun testCase -> testCase.Caption = "NotEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledNot = Disassemble (CorrectNotTestCase.Data, 0x00u)
-        Assert.True(DisassembledNot.Instruction.Opcode = NotInstruction.Opcode)
+        let DisasmOutput = Disassemble (CorrectNotTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledNot ->
+                Assert.True(disassembledNot.Instruction.Opcode = NotInstruction.Opcode)
+            | Error errorMessage -> Assert.True(false)
 
     [<Fact>]
     let ``Eq Instruction is disassembled correctly`` () =
         let CorrectEqTestCase = Array.head (DisassembleFunctionTestCases
                     |> Array.filter (fun testCase -> testCase.Caption = "EqEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledEq = Disassemble (CorrectEqTestCase.Data, 0x00u)
-        Assert.True(DisassembledEq.Instruction.Opcode = EqInstruction.Opcode)
+        let DisasmOutput = Disassemble (CorrectEqTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledEq ->
+                Assert.True(disassembledEq.Instruction.Opcode = EqInstruction.Opcode)
+            | Error errorMessage -> Assert.True(false)
 
     [<Fact>]
     let ``If Instruction is disassembled correctly`` () =
         let CorrectIfTestCase = Array.head (DisassembleFunctionTestCases
                     |> Array.filter (fun testCase -> testCase.Caption = "IfEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledIf = Disassemble (CorrectIfTestCase.Data, 0x00u)
-        Assert.True(DisassembledIf.Instruction.Opcode = IfInstruction.Opcode)
+        let DisasmOutput = Disassemble (CorrectIfTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledIf ->
+                Assert.True(disassembledIf.Instruction.Opcode = IfInstruction.Opcode)
+            | Error errorMessage -> Assert.True(false)
 
     [<Fact>]
     let ``Br Instruction is disassembled correctly`` () =
         let CorrectBrTestCase = Array.head (DisassembleFunctionTestCases
                     |> Array.filter (fun testCase -> testCase.Caption = "BrEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledBr = Disassemble (CorrectBrTestCase.Data, 0x00u)
-        Assert.True(DisassembledBr.Instruction.Opcode = BrInstruction.Opcode)
+        let DisasmOutput = Disassemble (CorrectBrTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledBr ->
+                Assert.True(disassembledBr.Instruction.Opcode = BrInstruction.Opcode)
+            | Error errorMessage -> Assert.True(false)
 
     [<Fact>]
     let ``Store Instruction is disassembled correctly`` () =
         let CorrectStoreTestCase = Array.head (DisassembleFunctionTestCases
                     |> Array.filter (fun testCase -> testCase.Caption = "StoreEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledStore = Disassemble (CorrectStoreTestCase.Data, 0x00u)
-        Assert.True(DisassembledStore.Instruction.Opcode = StoreInstruction.Opcode
-        && DisassembledStore.Operand = enum<Register>(int32 CorrectStoreTestCase.Data.[1]).ToString()
-        )
+        let DisasmOutput = Disassemble (CorrectStoreTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledStore ->
+                Assert.True(disassembledStore.Instruction.Opcode = StoreInstruction.Opcode
+                && disassembledStore.Operand = enum<Register>(int32 CorrectStoreTestCase.Data.[1]).ToString()
+                )
+            | Error errorMessage -> Assert.True(false)
 
     [<Fact>]
     let ``Load Instruction is disassembled correctly`` () =
         let CorrectLoadTestCase = Array.head (DisassembleFunctionTestCases
                     |> Array.filter (fun testCase -> testCase.Caption = "LoadEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledLoad = Disassemble (CorrectLoadTestCase.Data, 0x00u)
-        Assert.True(DisassembledLoad.Instruction.Opcode = LoadInstruction.Opcode
-        && DisassembledLoad.Operand = enum<Register>(int32 CorrectLoadTestCase.Data.[1]).ToString()
-        )
+        let DisasmOutput = Disassemble (CorrectLoadTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledLoad ->
+                Assert.True(disassembledLoad.Instruction.Opcode = LoadInstruction.Opcode
+                && disassembledLoad.Operand = enum<Register>(int32 CorrectLoadTestCase.Data.[1]).ToString()
+                )
+            | Error errorMessage -> Assert.True(false)
 
     [<Fact>]
     let ``Nop Instruction is disassembled correctly`` () =
         let CorrectNopTestCase = Array.head (DisassembleFunctionTestCases
                     |> Array.filter (fun testCase -> testCase.Caption = "NopEncoded" && testCase.Category = CorrectlyEncodedInstruction))
-        let DisassembledNop = Disassemble (CorrectNopTestCase.Data, 0x00u)
-        Assert.True(DisassembledNop.Instruction.Opcode = NopInstruction.Opcode)
+        let DisasmOutput = Disassemble (CorrectNopTestCase.Data, 0x00u)
+        match DisasmOutput with
+            | Ok disassembledNop ->
+                Assert.True(disassembledNop.Instruction.Opcode = NopInstruction.Opcode)
+            | Error errorMessage -> Assert.True(false)
 
     [<Fact>]
     let ``Linear sweep disassembly algorithm is implemented correctly`` () =
         let TestCaseData = Array.concat (Seq.ofArray (DisassembleLinearlyFunctionTestCase |> Array.choose (fun testCase -> Some (testCase.Data.Blob))))
         let TestCaseDisassembly = List.ofArray (DisassembleLinearlyFunctionTestCase |> Array.choose (fun testCase -> Some (testCase.Data)))
         let ImplementationResult = DisassembleLinearly (TestCaseData)
-        let DisassemblyOutputsAreIdentical = List.forall2 (fun disasm1 disasm2 -> disasm1 = disasm2) TestCaseDisassembly ImplementationResult
-        Assert.True(DisassemblyOutputsAreIdentical)
+        match ImplementationResult with
+            | Ok linearDisassembly ->
+                let DisassemblyOutputsAreIdentical = List.forall2 (fun disasm1 disasm2 -> disasm1 = disasm2) TestCaseDisassembly linearDisassembly
+                Assert.True(DisassemblyOutputsAreIdentical)
+            | Error errorMessage -> Assert.True(false)
--- SmokeStackVM/VMDisassembler.fs
+++ SmokeStackVM/VMDisassembler.fs
@@ -26,34 +26,43 @@ module VMDisassembler =
     
     let Disassemble (blob:byte [], instructionAddress:uint32) =
         let PC = 0
-        let DecodedInstruction = VMInstructionSet.[int blob.[PC]]
-        let (InstructionOperand:string, InstructionBlob:byte []) =
-            match DecodedInstruction.OperandType with
-            | RegisterOp -> enum<Register>(int32 blob.[PC + 1]).ToString(), blob.[PC .. PC + 1]
-            | ImmediateOp -> (uint32 blob.[PC + 1]).ToString(), blob.[PC .. PC + 1]
-            | NoneOp -> "", blob.[PC .. PC]
-        let DisassembledInstruction : DisassembledInstruction = {
-            Instruction = DecodedInstruction
-            Operand = InstructionOperand
-            Address = instructionAddress
-            Blob = InstructionBlob
-            }
-        DisassembledInstruction
-
-    let DisassembleLinearly (blob:byte []) : DisassembledInstruction list =
-        let rec DisassembleUsingTailCall (blob:byte [], pcTrackingInfo : PCTrackingInformation, disassembly: DisassembledInstruction list) =
-            if pcTrackingInfo.CurrentPC <= pcTrackingInfo.EndAddress then
-                let DisasmInstruction = Disassemble (blob.[int pcTrackingInfo.CurrentPC .. ], pcTrackingInfo.CurrentPC)
-                let InstructionSize = CalculateInstructionSize DisasmInstruction.Instruction
-                let UpdatedPCTrackingInfo = { 
-                    pcTrackingInfo with
-                        CurrentPC = pcTrackingInfo.CurrentPC + (uint32 InstructionSize);
-                        PreviousPC = pcTrackingInfo.CurrentPC;
-                        CurrentInstructionSize = InstructionSize;
+        let Opcode = blob.[PC]
+        if (int Opcode) > (VMInstructionSet.Length - 1) then Error (sprintf "Illegal instruction opcode: 0x%X" Opcode) else
+        let DecodedInstruction = VMInstructionSet.[int Opcode]
+        if (DecodedInstruction.OperandType = RegisterOp || DecodedInstruction.OperandType = ImmediateOp) && (blob.Length < 2) then
+            Error (sprintf "Illegal instruction length: %d" blob.Length)
+        else
+            let (InstructionOperand:string, InstructionBlob:byte []) =
+                match DecodedInstruction.OperandType with
+                    | RegisterOp -> enum<Register>(int32 blob.[PC + 1]).ToString(), blob.[PC .. PC + 1]
+                    | ImmediateOp -> (uint32 blob.[PC + 1]).ToString(), blob.[PC .. PC + 1]
+                    | NoneOp -> "", blob.[PC .. PC]
+            let DisassembledInstruction : DisassembledInstruction = {
+                Instruction = DecodedInstruction
+                Operand = InstructionOperand
+                Address = instructionAddress
+                Blob = InstructionBlob
                 }
-                DisassembleUsingTailCall (blob, UpdatedPCTrackingInfo , List.append disassembly [ DisasmInstruction ])
+            Ok DisassembledInstruction
+
+    let DisassembleLinearly (blob:byte []) =
+        let rec DisassembleUsingTailCall (blob:byte [], pcTrackingInfo : PCTrackingInformation, disassembly: DisassembledInstruction list) =
+            if blob.Length = 0 then Error (sprintf "Invalid blob buffer length: %d" blob.Length) else
+            if pcTrackingInfo.CurrentPC <= pcTrackingInfo.EndAddress then
+                let DisasmOutput = Disassemble (blob.[int pcTrackingInfo.CurrentPC .. ], pcTrackingInfo.CurrentPC)
+                match DisasmOutput with
+                    | Ok disasmInstruction ->
+                        let InstructionSize = CalculateInstructionSize disasmInstruction.Instruction
+                        let UpdatedPCTrackingInfo = { 
+                            pcTrackingInfo with
+                                CurrentPC = pcTrackingInfo.CurrentPC + (uint32 InstructionSize);
+                                PreviousPC = pcTrackingInfo.CurrentPC;
+                                CurrentInstructionSize = InstructionSize;
+                        }
+                        DisassembleUsingTailCall (blob, UpdatedPCTrackingInfo , List.append disassembly [ disasmInstruction ])
+                    | Error errorMessage -> Error errorMessage
             else
-                disassembly
+                Ok disassembly
 
         let PCTrackingInfo : PCTrackingInformation = {
             CurrentPC = 0u;
