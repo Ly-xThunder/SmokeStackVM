bfcfa5adc477657a6e630eb6b0a615a9467449a1
Malik Ashebani <malikashebani@gmail.com>
2019-11-26 17:05:59 +0200

    Implemented a generic Control Flow Graph data structure

---
+++ SmokeStackVM/CFGRecovery.fs
@@ -0,0 +1,16 @@
+﻿namespace SmokeStackVM.Analysis
+
+module CFGRecovery =
+    type CFGEdges<'TAddress> =
+        | Unary of TrueNode:'TAddress
+        | Binary of TrueNode:'TAddress * FalseNode:'TAddress
+        | None
+
+    type CFGNode<'TAddress, 'TExpressions> = {
+        Address: 'TAddress;
+        Label: string;
+        Expressions: 'TExpressions;
+        OutgoingEdges: CFGEdges<'TAddress>
+    }
+
+    type CFG<'TAddress, 'TExpressions> = CFGNode<'TAddress, 'TExpressions> list
