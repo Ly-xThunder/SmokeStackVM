82b39dc62b34177a44932f60415a2b91e3984472
Malik Ashebani <malikashebani@gmail.com>
2019-11-28 17:34:17 +0200

    Created 'bytePatternComparer' function

--- SmokeStackVM/CFGRecovery.fs
+++ SmokeStackVM/CFGRecovery.fs
@@ -1,5 +1,7 @@
 ﻿namespace SmokeStackVM.Analysis
 
+open PatternFinder
+
 module CFGRecovery =
     type CFGEdges<'TAddress> =
         | UnaryEdge of TrueNode:'TAddress
@@ -14,3 +16,14 @@ module CFGRecovery =
     }
 
     type CFG<'TAddress, 'TExpressions> = CFGNode<'TAddress, 'TExpressions> list
+
+    let private bytePatternComparer (searchSpace:array<_>, pattern:array<_>) =
+        let rec compareAndCount (searchSpace:array<_>, pattern:array<_>, matchesCount) =
+            if searchSpace.Length > 0 then
+                if (searchSpace.[0] = pattern.[0]) || pattern.[0] = None then
+                    compareAndCount (Array.tail searchSpace, Array.tail pattern, matchesCount + 1)
+                else
+                    matchesCount
+            else
+                matchesCount
+        compareAndCount (searchSpace, pattern, 0) = searchSpace.Length
